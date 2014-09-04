d72e73f1d3e16f07cadd850e7914f6ee3125b03f
MattBirmingham <Matt@Matts-MacBook-Pro.local>
2014-09-04 12:37:10 -0700

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Matt Birmingham on 9/4/14.
+//  Copyright (c) 2014 Cubit Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
